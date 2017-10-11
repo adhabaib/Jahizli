92f979bddcb8ebf4fabbf02538e03925db08d825
Abdullah <aldhabaib94@gmail.com>
2017-10-11 22:25:11 +0300

    FKMenu: Created basic wireframe of class and synced with both repo's!

---
+++ Jahizli/FKModelClasses/FKMenu.swift
@@ -0,0 +1,38 @@
+//
+//  FKMenu.swift
+//  Jahizli
+//
+//  Created by Abdullah Al Dhabaib on 10/11/17.
+//  Copyright © 2017 FekaTech. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// FKMenu Class
+class FKMenu: NSObject {
+    
+    // public variables
+    
+    var id: String = "?"
+    var menuCategories = [String]()
+    var menuItemIDs = [String]()
+    
+    
+    // public notification tags
+    
+    
+    
+    // Setup Object Function
+    
+    
+    
+    // Firebase Real-time Database Methods
+    
+    
+    
+    // Helper Methods
+    
+    
+}
+

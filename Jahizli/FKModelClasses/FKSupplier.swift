329702e9f6a1633abee5ae695144497877c3dd0e
Abdullah <aldhabaib94@gmail.com>
2017-10-15 22:56:10 +0300

    FKSupplier: Created class with public variables!

---
+++ Jahizli/FKModelClasses/FKSupplier.swift
@@ -0,0 +1,41 @@
+//
+//  FKSupplier.swift
+//  Jahizli
+//
+//  Created by Abdullah Al Dhabaib on 10/15/17.
+//  Copyright © 2017 FekaTech. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+//FKSupplier Class
+class FKSupplier: NSData {
+    
+    // public variables
+    var id : String = ""
+    var name : String = ""
+    var status: String = ""
+    var hours: String = ""
+    var info : String = ""
+    var phone_number: String = ""
+    
+    
+    var logo : Data!
+    var displayImage : Data!
+    
+    var balance: Double = 0.0
+    var creditRate: Double = 0.0
+    
+    var menu: FKMenu!
+    
+    // notification tags
+    
+    
+    
+    
+    
+    
+    
+    
+}
